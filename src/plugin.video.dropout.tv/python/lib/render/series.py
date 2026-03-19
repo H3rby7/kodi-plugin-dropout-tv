6c010a8febb07216b69648f3ae99e94ab4543f93
h3rby7 <herbielutz@gmx.net>
2026-03-19 22:27:18 +0100

    Exemplary idea to route through app with render_series

--- src/plugin.video.dropout.tv/python/lib/render/series.py
+++ src/plugin.video.dropout.tv/python/lib/render/series.py
@@ -9,4 +9,4 @@ def render_series(constants: PluginConstants, item: Item):
   # TODO: Slug should not be empty at this point but hey...
   li = xbmcgui.ListItem(item['name'])
   li.setArt({'thumb': item['thumbnail']['large']})
-  xbmcplugin.addDirectoryItem(handle=constants.addon_handle, url=f"plugin://{slug}", listitem=li, isFolder=True)
+  xbmcplugin.addDirectoryItem(handle=constants.addon_handle, url=f"{constants.base_url}?slug={slug}&type=collection&id={item['id']}", listitem=li, isFolder=True)
